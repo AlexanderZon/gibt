8ec15acf85a5939d3b08d702edb34c94d4a0c96f
AlexanderZon <amontenegro.sistemas@gmail.com>
2022-12-17 10:04:03 -0500

    Corrije tabla associations

--- database/migrations/2022_11_26_144827_create_associations_table.php
+++ database/migrations/2022_11_26_144827_create_associations_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('associations', function (Blueprint $table) {
+            $table->id();
             $table->string('name');
             $table->string('icon')->nullable();
             $table->unsignedInteger('order');
